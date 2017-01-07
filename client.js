6d3b91d6c265c9172a651b2b47df12a444145a52
Kaveh Khorram <kavehkhorram@Kavehs-MacBook-Pro.local>
2017-01-07 02:08:51 -0800

    Switched from Callbacks to Promises.

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {MyInput, valueInputed, myNotes} from './components/CustomForm.jsx';
 import App from './components/App.jsx';
@@ -6,7 +6,7 @@ import App from './components/App.jsx';
 import { Router, Route, Link, browserHistory, Redirect } from 'react-router';
 import { routes } from './client/routes';
 
-class ShoppingList extends React.Component {
+class ShoppingList extends Component {
   render() {
     return (
       <div className="shopping-list">
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var bodyParser = require('body-parser');
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
 var mongoose = require('mongoose');
+// set Promise provider to bluebird
+mongoose.Promise = require('bluebird');
 var passport = require('passport');
 var passportLocal = require('passport-local');
 var passportLocalMongoose = require('passport-local-mongoose');
@@ -38,51 +40,66 @@ app.set('view engine', 'ejs');
 
 var sortedRank = [];
 
-User.find({}, function(err,body){
+User.find().then((body) => { // mpromise is deprecated, use bluebird instead maybe..
+  if (body.length < 1) {
+    console.log(body);
+  } else {
+    var sortedArray = new LazyArray(body);
+    sortedArray.sort('score', 'ascending');
+    Object.keys(sortedArray).forEach((key) => {
+      sortedRank.push(sortedArray[key]);
+    })
+  }
+})
 
-  if(err){
-    console.log(err)
-  }else{
+// User.find({}, (err,body) => {
 
-      var sortedArray = new LazyArray(body);
-
-      sortedArray.sort("score", "ascending");
-
-
-      Object.keys(sortedArray).forEach(function(key) {
-
-          sortedRank.push(sortedArray[key]);
-
-      });
-
-
-    }
-
-});
+//   if(err){
+//     console.log(err);
+//   } else {
+//       var sortedArray = new LazyArray(body);
+//       sortedArray.sort("score", "ascending");
+//       Object.keys(sortedArray).forEach((key) => {
+//           sortedRank.push(sortedArray[key]);
+//       });
+//     }
+// });
 // // // // // // // // // // // // // // // // // //
 
 //The landing page.
-app.get('/', function(req,res){
+app.get('/', (req,res) => {
 
-  Questions.find({}, function(err,body){
-
-//Error in finding questions.
-    if(err){
-      console.log(err)
-    var randomQuestion = 'ERROR';
-
-      res.render('home', { randomQuestion : randomQuestion });
-//All questions are found.
-    }else{
-      //Randomize the questions.
-    var rand = uniqueRandomArray(body)
-      //Random questions to check.
-  var randomQuestion = rand();
-
-  res.render('home', { randomQuestion : rand() });
-
+  Questions.find().then((body) => {
+    if (body.length < 1) {
+      console.log(body);
+      const randomQuestion = 'ERROR';
+      res.render('home', { randomQuestion: randomQuestion });
+    } else {
+      var rand = uniqueRandomArray(body);
+      var randomQuestion = rand();
+      res.render('home', { randomQuestion: rand() });
     }
-  });
+  })
+
+//   Questions.find({}, function(err,body){
+
+// //Error in finding questions.
+//     if(err){
+//       console.log(err)
+//     var randomQuestion = 'ERROR';
+
+//       res.render('home', { randomQuestion : randomQuestion });
+// //All questions are found.
+//     }else{
+//       //Randomize the questions.
+//     var rand = uniqueRandomArray(body)
+//       //Random questions to check.
+//   var randomQuestion = rand();
+
+//   res.render('home', { randomQuestion : rand() });
+
+//     }
+//   });
 
   if(req.isAuthenticated()){
 
@@ -95,96 +112,152 @@ app.get('/', function(req,res){
 });
 
 //The way you find a user.
-app.get('/:user', function(req,res){
+app.get('/:user', (req,res) => {
 
   var userSearched = req.params.user;
 
       // Querying the database and getting the names //
 // // // // // // // // // // // // // // // // // // // // //
-User.find({ username : userSearched}, function(err,body){
-    if(err){
-      console.log(err);
+  User.find({ username: userSearched }).then((body) => {
+    if (body.length < 1) {
+      console.log(body);
       console.log('HAHAHAH');
-    }else{
-
-      //See if there is a username with a try statement.
+    } else {
       try {
-      var userName = body[0].username;
-      var userScore = body[0].score;
-      var answeredIds = body[0].answeredQs;
-
-
+        const userName = body[0].username;
+        const userScore = body[0].score;
+        const answeredIds = body[0].answeredQs;
       } catch (e) {
-        //userName = 'Invalid Bullshit';
-      //  res.render('user/cannotfind');
+        // Redirect to some `cannotfind` page.
       } finally {
-        // Code that goes if success.
+        // success? :-)
       }
 
-if(userName == undefined){
+      if (userName === undefined) {
+        res.render('user/cannotfind');
+      } else {
+        var questions = {
+          questionsID: [],
+          correct: 0,
+          wrong: 0
+        };
+        var questionsSaved = [];
+        answeredIds.forEach((qs) => {
+          switch (sq.status) {
+            case 'correct': 
+              questions.correct += 1;
+              break;
+            case 'wrong':
+              questions.wrong += 1;
+              break;
+          }
+          const questionId = qs.id;
+          questions.questionsID.push(questionId);
+        }); //answerIds forEach
 
- res.render('user/cannotfind');
+        Questions.find({ _id: { $in: questions.questionsID }}).then((markets) => {
+          if (markets.length < 1) {
+            console.log(markets);
+          } else {
+            // Do nothing here.
+          }
+          //If no error, we render your public profile.
+          res.render('user/public-profile', {
+            userName : userName,
+            userScore : userScore,
+            answeredQuestions : markets,
+            answeredCorrect : questions.correct,
+            answeredWrong : questions.wrong
+          }); // res.render
+        }) // Questions promise
+      } // else (child)
+    } // else (parent)
+  }) // Promise (.then)
 
-}else {
+// User.find({ username : userSearched}, function(err,body){
+//     if(err){
+//       console.log(err);
+//       console.log('HAHAHAH');
+//     }else{
 
-
-  var questions = {
-    questionsID : [],
-    correct : 0,
-    wrong : 0
-  };
-
-  var questionsSaved = [];
-
-
-answeredIds.forEach(function(qs){
-
-
-if(qs.status == 'correct'){
-  questions.correct += 1
-}
-
-if(qs.status == 'wrong'){
-  questions.wrong += 1
-}
-
-var questionId = qs.id;
-
-questions.questionsID.push(questionId);
-
-
-})
-
-
-    //Find the matching questions.
-Questions.find({ _id: { $in: questions.questionsID }}, function (err, markets) {
-
-
-if(err){
-  console.log(err)
-}else{
-  //Do nothing here.
-}
-
-
-//If no error, we render your public profile.
-    res.render('user/public-profile',{
-        userName : userName,
-        userScore : userScore,
-        answeredQuestions : markets,
-        answeredCorrect : questions.correct,
-        answeredWrong : questions.wrong
-      });
+//       //See if there is a username with a try statement.
+//       try {
+//       var userName = body[0].username;
+//       var userScore = body[0].score;
+//       var answeredIds = body[0].answeredQs;
 
 
-})
+//       } catch (e) {
+//         //userName = 'Invalid Bullshit';
+//       //  res.render('user/cannotfind');
+//       } finally {
+//         // Code that goes if success.
+//       }
+
+// if(userName == undefined){
+
+//  res.render('user/cannotfind');
+
+// }else {
+
+
+//   var questions = {
+//     questionsID : [],
+//     correct : 0,
+//     wrong : 0
+//   };
+
+//   var questionsSaved = [];
+
+
+// answeredIds.forEach(function(qs){
+
+
+// if(qs.status == 'correct'){
+//   questions.correct += 1
+// }
+
+// if(qs.status == 'wrong'){
+//   questions.wrong += 1
+// }
+
+// var questionId = qs.id;
+
+// questions.questionsID.push(questionId);
+
+
+// })
+
+
+//     //Find the matching questions.
+// Questions.find({ _id: { $in: questions.questionsID }}, function (err, markets) {
+
+
+// if(err){
+//   console.log(err)
+// }else{
+//   //Do nothing here.
+// }
+
+
+// //If no error, we render your public profile.
+//     res.render('user/public-profile',{
+//         userName : userName,
+//         userScore : userScore,
+//         answeredQuestions : markets,
+//         answeredCorrect : questions.correct,
+//         answeredWrong : questions.wrong
+//       });
+
+
+// })
 
 
 
-    }
+//     }
 
-  }
-})
+//   }
+// })
 // // // // // // // // // // // // // // // // // // // // //
 
 });
@@ -194,37 +267,39 @@ if(err){
 mongoose.connect('mongodb://farhan:farhan@ds145677.mlab.com:45677/eloify_db');
 
 //The pages rendered with React.js
-app.get(['/mapgame', '/react-login', '/react-register', '/mapgame2'], function(req,res){
+app.get(['/mapgame', '/react-login', '/react-register', '/mapgame2'], (req, res) => {
 
-var ReactRouter = require('react-router');
-var match = ReactRouter.match;
-var routes = require('./client/routes').routes;
-var RouterContext = React.createFactory(ReactRouter.RouterContext);
+    var ReactRouter = require('react-router');
+    var match = ReactRouter.match;
+    var routes = require('./client/routes').routes;
+    var RouterContext = React.createFactory(ReactRouter.RouterContext);
 
-    match({routes: routes, location: req.url}, function(error, redirectLocation, renderProps) {
-      if (error) {
-        res.status(500).send(error.message)
-      } else if (redirectLocation) {
-        res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-      } else if (renderProps) {
+    match({
+        routes: routes,
+        location: req.url
+    }, (error, redirectLocation, renderProps) => {
+        if (error) {
+            res.status(500).send(error.message)
+        } else if (redirectLocation) {
+            res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+        } else if (renderProps) {
 
-//This is how it would work with redux
-        // res.send("<!DOCTYPE html>"+
-        //   ReactDOMServer.renderToString(
-        //     Provider({store: store}, RouterContext(renderProps))
-        //   )
-        // );
+            //This is how it would work with redux
+            // res.send("<!DOCTYPE html>"+
+            //   ReactDOMServer.renderToString(
+            //     Provider({store: store}, RouterContext(renderProps))
+            //   )
+            // );
 
-      res.send("<!DOCTYPE html>"+
-          ReactDOMServer.renderToString( RouterContext(renderProps)
-          )
-        );
+            res.send("<!DOCTYPE html>" +
+                ReactDOMServer.renderToString(RouterContext(renderProps))
+            );
 
-        //res.send('React Route MATCHED!!!!');
+            //res.send('React Route MATCHED!!!!');
 
-      } else {
-        res.status(404).send('Not found')
-      }
+        } else {
+            res.status(404).send('Not found')
+        }
     });
 
 });
@@ -236,7 +311,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Listen on PORT 3000
 app.listen(process.env.PORT || '3000', function(){
-
   console.log('Listening on PORT 3000');
-
 });
